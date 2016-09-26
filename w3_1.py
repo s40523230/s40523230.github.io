3291170f6b5f03106bf9a16e43231f1ebb87e56c
s40523230 <40523230@gm.nfu.edu.tw>
2016-09-26 16:04:08 +0800

    Updated from Brython Server: 2016/9/26 下午4:04:08

--- w3_1.py
+++ w3_1.py
@@ -1,10 +1,5 @@
 print("老師")
 print("你知道女生最性感的瞬間是什麼嗎?")
 print("答案就是")
-answer = 123
-while True:
-    str_num = input('請輸入123:     ')
-    int_num = int(str_num)
-    if int_num==answer:
-        print('女生從裙子底下掏出大jj的時候')
-        break
+下堂課揭曉 = input("123")
+print (下堂課揭曉)
